da81efa08e54b1ff4c8ba14e70e88ab0d0fbd18a
adamdawkins <adam@dragondrop.uk>
2024-12-15 22:22:24 +0000

    Add login

--- app/controllers/adam_controller.rb
+++ app/controllers/adam_controller.rb
@@ -2,4 +2,14 @@ class AdamController < ApplicationController
   # GET /adam/new
   def new
   end
+
+  # POST /adam
+  def create
+    if params[:password] == Rails.application.credentials.password
+      session[:adam] = true
+      redirect_to root_url
+    else
+      render :new, status: :unauthorized
+    end
+  end
 end
--- spec/requests/adam_spec.rb
+++ spec/requests/adam_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe "/adam", type: :request do
       expect(response).to be_successful
     end
   end
+
+  describe "POST /adam" do
+    context "when the password matches the password credential" do
+      it "redirects to the root page" do
+        post adam_url, params: { password: "password" }
+        expect(response).to redirect_to(root_url)
+        follow_redirect!
+        expect(session[:adam]).to be_truthy
+      end
+    end
+    context "when the password does not match the password cerdential" do
+      it "renders a response with the 422 status code" do
+        post adam_url, params: { password: "wrong" }
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
 end
