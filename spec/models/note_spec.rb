3fa411f1d9935d42f78d31e291e01f8362df4081
adamdawkins <adam@dragondrop.uk>
2024-12-15 22:52:41 +0000

    Validate presence of note body

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a body" do
+    expect(Note.new(body: "Hello")).to be_valid
+  end
+
+  it "is not valid without a body" do
+    expect(Note.new(body: nil)).to_not be_valid
+  end
 end
