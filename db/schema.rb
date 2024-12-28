6de9eb07f46a858f8e1e37224879df78de8a4812
adamdawkins <adam@dragondrop.uk>
2024-12-28 17:35:54 +0000

    Enable pg_catalog.plpgsql in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_15_211357) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.text "body"
     t.datetime "published_at"
